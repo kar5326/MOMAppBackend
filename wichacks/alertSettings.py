a08f2f9baabd730b5a94aa931072bfb5ab6a0961
Kiersten Reina <kar5326@rit.edu>
2019-03-02 18:52:35 -0500

    I did stuff!!! I created all of the notifications and wrote the getter and setters

--- wichacks/alertSettings.py
+++ wichacks/alertSettings.py
@@ -1,18 +1,91 @@
 from dataclasses import dataclass
-from wichacks import alert_type
+from wichacks import alertType
 
 @dataclass
 class Settings:
     name: str  # name of alert
-    alert_type: alert_type.Type  # type fo alert e.g. health, hygiene
+    alert_type: alertType.Type  # type fo alert e.g. health, hygiene
     alert_time: int  # alert time
     enabled: bool  # whether the alert is enabled
 
 
 def create_alert(name, type, time, enable):
-    #  Settings.name = name
+    """
+    Constructor for Settings
+    :param name: name of alert
+    :param type: type of alert
+    :param time: time alert goes off
+    :param enable: whether alert is set to go off
+    :return: New settings object
+    """
     Settings.alert_type = type
-    #  Settings.alert_time = time
-    #  Settings.enabled = enable
     return Settings(name, type, time, enable)
+
+
+def set_name(name):
+    """
+    Rename alert
+    :param name: name of the alert
+    :return: None
+    """
+    Settings.name = name
+
+
+def set_type(type):
+    """
+    Reset type of alert
+    :param type: the type the alert is being changed with
+    :return: None
+    """
+    Settings.alert_type = alertType.set_type(type)
+
+
+def set_time(time):
+    """
+    Reset time the alert will go off
+    :param time: Time for the alert
+    :return: None
+    """
+    Settings.alert_time = time
+
+
+def set_enabled(state):
+    """
+    Change if the alert is selected to go off
+    :param state: true or false
+    :return: None
+    """
+    Settings.enabled = state
+
+
+def get_name():
+    """
+    Get name of the alert
+    :return: the name
+    """
+    return Settings.name
+
+
+def get_type():
+    """
+    Get the type of the alert
+    :return: the type
+    """
+    return Settings.alert_type
+
+
+def get_time():
+    """
+    Get what time the alert goes off
+    :return: the time
+    """
+    return Settings.alert_time
+
+
+def get_enabled():
+    """
+    Get whether the alert is set to go off
+    :return: true if the alert will go off, false if not
+    """
+    return Settings.enabled
 
--- wichacks/alerts.py
+++ wichacks/alerts.py
@@ -1,47 +1,46 @@
 from wichacks import alertSettings
-from wichacks import alert_type
+from wichacks import alertType
 
 
 def sleep():
-    alertSettings.create_alert("Sleep", alert_type.set_type(alert_type.Type.HEALTH), 2000, True)
-    print(alert_type.set_type(alert_type.Type.HEALTH))
+    alertSettings.create_alert("Sleep", alertType.set_type(alertType.Type.HEALTH), 2000, True)
 
 
 def hygiene():
-    alertSettings.create_alert("Hygiene", alert_type.set_type(alert_type.Type.HEALTH), 2000, True)
+    alertSettings.create_alert("Hygiene", alertType.set_type(alertType.Type.HEALTH), 2000, True)
 
 
 def exercise():
-    alertSettings.create_alert("Exercise", alert_type.set_type(alert_type.Type.HEALTH), 2000, True)
+    alertSettings.create_alert("Exercise", alertType.set_type(alertType.Type.HEALTH), 2000, True)
 
 
 def drink():
-    alertSettings.create_alert("Drink Water", alert_type.set_type(alert_type.Type.EATING), 2000, True)
+    alertSettings.create_alert("Drink Water", alertType.set_type(alertType.Type.EATING), 2000, True)
 
 
 def make_food():
-    alertSettings.create_alert("Cook Dinner", alert_type.set_type(alert_type.Type.HEALTH), 2000, True)
+    alertSettings.create_alert("Cook Dinner", alertType.set_type(alertType.Type.HEALTH), 2000, True)
 
 
 def shopping():
-    alertSettings.create_alert("Shopping List", alert_type.set_type(alert_type.Type.EATING), 2000, True)
+    alertSettings.create_alert("Shopping List", alertType.set_type(alertType.Type.EATING), 2000, True)
 
 
 def recipe():
-    alertSettings.create_alert("Daily Recipe", alert_type.set_type(alert_type.Type.EATING), 2000, True)
+    alertSettings.create_alert("Daily Recipe", alertType.set_type(alertType.Type.EATING), 2000, True)
 
 
 def calendar():
-    alertSettings.create_alert("Calendar", alert_type.set_type(alert_type.Type.SCHEDULE), 2000, True)
+    alertSettings.create_alert("Calendar", alertType.set_type(alertType.Type.SCHEDULE), 2000, True)
 
 
 def work():
-    alertSettings.create_alert("Work Schedule", alert_type.set_type(alert_type.Type.SCHEDULE), 2000, True)
+    alertSettings.create_alert("Work Schedule", alertType.set_type(alertType.Type.SCHEDULE), 2000, True)
 
 
 def encouragement():
-    alertSettings.create_alert("Encouragement", alert_type.set_type(alert_type.Type.EXTRA), 2000, True)
+    alertSettings.create_alert("Encouragement", alertType.set_type(alertType.Type.EXTRA), 2000, True)
 
 
 def phone_time():
-    alertSettings.create_alert("Phone Time", alert_type.set_type(alert_type.Type.EXTRA), 2000, True)
+    alertSettings.create_alert("Phone Time", alertType.set_type(alertType.Type.EXTRA), 2000, True)
